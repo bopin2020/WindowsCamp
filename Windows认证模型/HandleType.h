f96bb6e9746d39f632d80e8a1523c9fa84637d86
bopin <49940777+bopin2020@users.noreply.github.com>
2023-01-06 19:50:29 +0800

    Update HandleType.h

--- Windows认证模型/HandleType.h
+++ Windows认证模型/HandleType.h
@@ -1,17 +1,27 @@
 // NtQuerySystemInformation 枚举全局句柄表 搜索句柄类型，权限，句柄值
+// https://github.com/CLRDivingNET/AllTools
+// Kernel Object Viewer
 typedef enum HANDLETYPE
 {
+    Type = 2,
     Directory = 3,
+    SymbolicLink = 4,
     Token = 5,
     Job = 6,
     Process = 7,
     Thread = 8,         // 
     Partition = 9,        // 存储分区
+    UserApcReserve = 10,
+    DebugObject = 17,
     Event = 18,         // \KernelObjects
     Mutant = 19,
+    Callback = 20,
     Semaphore = 21,     // \BaseNamedObject
+    Timer = 22,
     WindowStation = 26, // \Window\WindowStations\Winsta0
     Desktop = 27,       // \Default
+    Device = 35,
+    Driver = 36,
     File = 39,
     Section = 45,       // \\BaseNamedObject
     RegisterKey = 47,
